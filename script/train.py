d11026acef32451abaf7b6541baabc6e28203470
Vincentius Verel <vincentiusverel@Vincentiuss-MacBook-Air.local>
2024-04-23 13:47:59 +0700

    create custom model 3

--- script/train.py
+++ script/train.py
@@ -39,6 +39,50 @@ def create_custom_model_2():
     ])
     return model
 
+def create_custom_model_3():
+    model = tf.keras.models.Sequential([
+        # First block
+        tf.keras.layers.Conv2D(64, (3, 3), padding='same', activation='relu', input_shape=(48, 48, 1)),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.Conv2D(64, (3, 3), padding='same', activation='relu'),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.Conv2D(64, (3, 3), padding='same', activation='relu'),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.Dropout(0.2),
+        
+        # Four blocks
+        tf.keras.layers.Conv2D(64, (3, 3), padding='same', activation='relu'),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.Conv2D(64, (3, 3), padding='same', activation='relu'),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.MaxPooling2D(),
+        tf.keras.layers.Dropout(0.3),
+        
+        tf.keras.layers.Conv2D(64, (3, 3), padding='same', activation='relu'),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.Conv2D(64, (3, 3), padding='same', activation='relu'),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.MaxPooling2D(),
+        tf.keras.layers.Dropout(0.3),
+        
+        # Downsampling
+        tf.keras.layers.Conv2D(64, (3, 3), strides=(2, 2), padding='same', activation='relu'),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.Conv2D(64, (3, 3), padding='same', activation='relu'),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.MaxPooling2D(),
+        tf.keras.layers.Dropout(0.4),
+        
+        # Classification block
+        tf.keras.layers.Conv2D(64, (1, 1), activation='relu'),
+        tf.keras.layers.Conv2D(8, (1, 1)),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dropout(0.5),
+        tf.keras.layers.Dense(8, activation='softmax')
+    ])
+    
+    return model
+
 def create_vgg16_model():
     base_model = tf.keras.applications.VGG16(
         weights='imagenet',  # Load weights pre-trained on ImageNet.
@@ -47,6 +91,23 @@ def create_vgg16_model():
 
     # Freeze the base model
     base_model.trainable = False
+
+    # Create new model on top
+    model = tf.keras.Sequential([
+        base_model,
+        tf.keras.layers.GlobalAveragePooling2D(),
+        tf.keras.layers.Dense(8, activation='softmax')
+    ])
+    return model
+
+def create_vgg16_model_trainable():
+    base_model = tf.keras.applications.VGG16(
+        weights='imagenet',  # Load weights pre-trained on ImageNet.
+        input_shape=(48, 48, 3),  # Shape of input image.
+        include_top=False)  # Do not include the ImageNet classifier at the top.
+
+    # Freeze the base model
+    base_model.trainable = True
 
     # Create new model on top
     model = tf.keras.Sequential([
@@ -73,6 +134,23 @@ def create_resnet_model():
     ])
     return model
 
+def create_resnet_model_trainable():
+    base_model = tf.keras.applications.ResNet50(
+        weights='imagenet',  # Load weights pre-trained on ImageNet.
+        input_shape=(48, 48, 3),  # Shape of input image.
+        include_top=False)  # Do not include the ImageNet classifier at the top.
+
+    # Freeze the base model
+    base_model.trainable = True
+
+    # Create new model on top
+    model = tf.keras.Sequential([
+        base_model,
+        tf.keras.layers.GlobalAveragePooling2D(),
+        tf.keras.layers.Dense(8, activation='softmax')
+    ])
+    return model
+
 def create_inceptionv3_model():
     base_model = tf.keras.applications.InceptionV3(
         weights='imagenet',  # Load weights pre-trained on ImageNet.
@@ -81,6 +159,23 @@ def create_inceptionv3_model():
 
     # Freeze the base model
     base_model.trainable = False
+
+    # Create new model on top
+    model = tf.keras.Sequential([
+        base_model,
+        tf.keras.layers.GlobalAveragePooling2D(),
+        tf.keras.layers.Dense(8, activation='softmax')
+    ])
+    return model
+
+def create_inceptionv3_model_trainable():
+    base_model = tf.keras.applications.InceptionV3(
+        weights='imagenet',  # Load weights pre-trained on ImageNet.
+        input_shape=(75, 75, 3),  # Shape of input image.
+        include_top=False)  # Do not include the ImageNet classifier at the top.
+
+    # Freeze the base model
+    base_model.trainable = True
 
     # Create new model on top
     model = tf.keras.Sequential([
@@ -118,6 +213,7 @@ def parse_arguments():
     parser = argparse.ArgumentParser(description='Training Parameters')
     parser.add_argument('-dataset', type=str, default='ori', help='Name of the dataset to use (ori, augmented, masked)', required=True)
     parser.add_argument('-model', type=str, default='custom1', help='Name of the model to train (vgg16, resnet, inceptionv3, custom1, custom2)', required=True)
+    parser.add_argument('-trainable', type=str, default='False', help='For VGG16, InceptionV3, and Resnet previous layer can be trainable or not (True or False)', required=False)
     parser.add_argument('-epochs', type=int, default=10, help='Nmber of epochs while training', required=True)
     parser.add_argument('-bs', type=int, default=32, help='Batch size while training', required=True)
     parser.add_argument('-lr', type=float, default=0.0001, help='Initial learning rate', required=True)
@@ -128,6 +224,7 @@ def parse_arguments():
 if __name__ == '__main__':
     args = parse_arguments()
     model_type = args.model
+    trainable = args.trainable
     dataset = args.dataset
     epochs = args.epochs
     batch_size = args.bs
@@ -151,18 +248,37 @@ if __name__ == '__main__':
         model = create_custom_model_2()
         color_mode = 'grayscale'
         target_size = (48, 48)
+    elif model_type == 'custom3':
+        model = create_custom_model_3()
+        color_mode = 'grayscale'
+        target_size = (48, 48)
     elif model_type == 'vgg16':
-        model = create_vgg16_model()
-        color_mode = 'rgb'
-        target_size = (48, 48)
+        if trainable == 'False':
+            model = create_vgg16_model()
+            color_mode = 'rgb'
+            target_size = (48, 48)
+        else:
+            model = create_vgg16_model_trainable()
+            color_mode = 'rgb'
+            target_size = (48, 48)
     elif model_type == 'resnet':
-        model = create_resnet_model()
-        color_mode = 'rgb'
-        target_size = (48, 48)
+        if trainable == 'False':
+            model = create_resnet_model()
+            color_mode = 'rgb'
+            target_size = (48, 48)
+        else:
+            model = create_resnet_model_trainable()
+            color_mode = 'rgb'
+            target_size = (48, 48)
     elif model_type == 'inceptionv3':
-        model = create_inceptionv3_model()
-        color_mode = 'rgb'
-        target_size = (75, 75)
+        if trainable == 'False':
+            model = create_inceptionv3_model()
+            color_mode = 'rgb'
+            target_size = (75, 75)
+        else:
+            model = create_inceptionv3_model_trainable()
+            color_mode = 'rgb'
+            target_size = (75, 75)
     else:
         raise ValueError("Invalid model type. Choose one of 'custom', 'vgg16', 'resnet', 'inceptionv3'.")
 
