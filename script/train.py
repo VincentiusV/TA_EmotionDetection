f9378ddc02c7dfc8171dec9f392fb8cccc436be8
Vincentius Verel <vincentiusverel@Vincentiuss-MacBook-Air.local>
2024-04-27 22:57:54 +0700

    update train

--- script/train.py
+++ script/train.py
@@ -27,11 +27,11 @@ def create_custom_model_2():
         tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(48, 48, 1)),
         tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
         tf.keras.layers.MaxPool2D(2, 2),
-        tf.keras.layers.Dropout(0.02),  #0.02
+        tf.keras.layers.Dropout(0.02),
         tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(48, 48, 1)),
         tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
         tf.keras.layers.MaxPool2D(2, 2),
-        tf.keras.layers.Dropout(0.5),
+        tf.keras.layers.Dropout(0.05),
         tf.keras.layers.Flatten(),
         tf.keras.layers.Dense(64, activation='relu'),
         tf.keras.layers.Dropout(0.2),
